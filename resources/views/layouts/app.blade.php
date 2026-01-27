6d9c697f4b764c48f9d2576c344f7151bd89c412
bee7er <contact_bee@yahoo.com>
2026-01-27 14:30:52 +0000

    Force client side relaod of css and jsp

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,9 +12,9 @@
     <!-- Fonts -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.4/css/bulma.min.css">
 
-    <link href="{{env("BASE_URL", "/")}}css/site.css?v21" rel="stylesheet">
+    <link href="{{env("BASE_URL", "/")}}css/site.css?v22" rel="stylesheet">
     <!-- Javascript -->
-    <script type="text/javascript" src="{{env("BASE_URL", "/")}}js/smb.js?v4"></script>
+    <script type="text/javascript" src="{{env("BASE_URL", "/")}}js/smb.js?v5"></script>
 </head>
 <body>
 
